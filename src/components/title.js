ac8c3fb739d9081a1ccf69eba77c6be2ef315948
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-12-05 14:11:32 -0800

    add moms japanese name under her english name

--- src/components/title.js
+++ src/components/title.js
@@ -17,10 +17,15 @@ class Title extends Component {
     return (
       <div className="row title-row">
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 title">
-          <h1 id="title-name">Keiko Iwako</h1>
+          <h1 id="title-name">
+            Keiko Iwako
+            <p id="title-japanese-name">
+              岩子敬子
+            </p>
+            </h1>
           <h1 id="title-slidedown-icon"
               style={{'cursor': 'pointer'}}
-              onClick={() => {this.scrollPageDown()}}><i className="fa fa-angle-double-down fa-lg"></i>
+              onClick={() => {this.scrollPageDown()}}><i className="fa fa-angle-double-up fa-lg"></i>
           </h1>
         </div>
       </div>
