ccbea82d8fe1dd7b0ead9103c9b17b71aa6a719d
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-12-05 10:14:18 -0800

    npm install bootstrap and font-awesome, import statement added to head of index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 class Website extends Component {
 
